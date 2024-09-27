5b251d0933a2ac74a5455ef1622085aeccc93e8a
Nicolás Krukowski <nicokrukowski@gmail.com>
2024-09-27 01:59:13 -0300

    DOM y cambio en el concepto

--- main.js
+++ main.js
@@ -4,40 +4,83 @@ let btnDel = document.getElementById("delete")
 let lista = document.getElementById("lista")
 
 function mostrarTareasGuardadas() {
-    let tareasGuardados = JSON.parse(localStorage.getItem('textos')) || []
 
+    let tareasGuardadas = JSON.parse(localStorage.getItem('tareas')) || []
+
+    
     lista.innerHTML = ''
 
-    // console.log(tareasGuardados)
-
-    tareasGuardados.forEach(function(texto) {
+    
+    tareasGuardadas.forEach(function(tarea) {
         let li = document.createElement('li')
-        li.textContent = texto
+        li.textContent = tarea.texto
+
+        
+        let checkbox = document.createElement('input');
+        checkbox.type = 'checkbox'
+        checkbox.checked = tarea.completada
+        checkbox.addEventListener('change', function() {
+            tarea.completada = checkbox.checked
+            guardarTareas(tareasGuardadas)
+        })
+
+        
+        li.prepend(checkbox)
+
+        
         lista.appendChild(li)
     })
 }
 
-mostrarTareasGuardadas()
+function guardarTareas(tareas) {
+    localStorage.setItem('tareas', JSON.stringify(tareas))
+}
 
 function agregar() {
     let tareaAgregada = inputText.value
 
     if (tareaAgregada) {
-        let tareasGuardados = JSON.parse(localStorage.getItem('textos')) || []
+        
+        let tareasGuardadas = JSON.parse(localStorage.getItem('tareas')) || []
 
-        tareasGuardados.push(tareaAgregada)
+        
+        let nuevaTarea = {
+            texto: tareaAgregada,
+            completada: false
+        }
 
-        localStorage.setItem('textos', JSON.stringify(tareasGuardados))
+        
+        tareasGuardadas.push(nuevaTarea)
 
+        
+        guardarTareas(tareasGuardadas)
+
+        
         mostrarTareasGuardadas()
 
+        
         inputText.value = ""
     }
 }
 
 function borrar() {
     
+    let tareasGuardadas = JSON.parse(localStorage.getItem('tareas')) || []
+
+    
+    let tareasRestantes = tareasGuardadas.filter(function(tarea) {
+        return !tarea.completada
+    })
+
+    
+    guardarTareas(tareasRestantes)
+
+    
+    mostrarTareasGuardadas()
 }
 
 document.getElementById("add").addEventListener("click", agregar)
 document.getElementById("delete").addEventListener("click", borrar)
+
+
+mostrarTareasGuardadas()
